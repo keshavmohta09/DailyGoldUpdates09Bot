8b46004d404b92980817788b9655494ecd026eaf
Keshav Mohta <68001229+keshavmohta09@users.noreply.github.com>
2026-08-10 01:56:36 +0530

    fix: support of multiple chat ids

--- config.py
+++ config.py
@@ -5,5 +5,16 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+CHAT_IDS = [
+
+    chat_id.strip()
+
+    for chat_id in os.getenv(
+        "TELEGRAM_CHAT_ID",
+        ""
+    ).split(",")
+
+    if chat_id.strip()
+
+]
 GOLD_API_KEY = os.getenv("GOLD_API_KEY")
